89b62caa5795acdbde74303f37f380d6d833e35e
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-03-09 02:52:38 +0100

    cw01: Add simple test with raport generation

--- cw01/zad2/find.c
+++ cw01/zad2/find.c
@@ -5,6 +5,7 @@
 #include <sys/times.h>
 #include <unistd.h>
 
+
 static clock_t st_time;
 static clock_t en_time;
 static struct tms st_cpu;
@@ -15,7 +16,10 @@ static char* log_filename = NULL;
 
 void clock_log(FILE* f, char* name) {
     int64_t clk_tck = sysconf(_SC_CLK_TCK);
-    fprintf(f, "\n%s\nReal: %.2fs\nUser: %.2fs\tChildren: %.2fs\nSystem: %.2fs\tChildren: %.2fs\n",
+    fprintf(f, "\n\n%s\n"
+               "Real:   %5.2fs\n"
+               "User:   %5.2fs  Children User: %5.2fs\n"
+               "System: %5.2fs  Children User: %5.2fs\n",
             name,
             (double)(en_time - st_time) / clk_tck,
             (double)(en_cpu.tms_utime - st_cpu.tms_utime) / clk_tck,
@@ -53,7 +57,7 @@ int main(int argc, char* argv[]) {
             if(i + 1 >= argc) break;
             fnd_init((size_t) atoi(argv[i + 1]));
             i += 1;
-        } else if(strcmp(argv[i], "search") == 0) {
+        } else if(strcmp(argv[i], "find") == 0) {
             if(i + 3 >= argc) break;
             fnd_prepare(argv[i + 1], argv[i + 2], argv[i + 3]);
             fnd_search();
@@ -72,7 +76,7 @@ int main(int argc, char* argv[]) {
             if(i + 1 >= argc) break;
             clock_stop(argv[i + 1]);
             i += 1;
-        } else if(strcmp(argv[i], "logfile") == 0) {
+        } else if(strcmp(argv[i], "log") == 0) {
             if(i + 1 >= argc) break;
             set_log(argv[i + 1]);
             i += 1;
