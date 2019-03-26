5748e7dc86e6844417e4440882c60b8b819367d3
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-03-26 10:40:57 +0100

    cw04: Add basic program halting

---
+++ cw04/zad1/halt.c
@@ -0,0 +1,52 @@
+#define _XOPEN_SOURCE 500
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+static int running = 1;
+
+void handle_ctrl_c(int s)
+{
+    printf("\tReceived Ctrl+C - terminating\n");
+    exit(0);
+}
+
+void handle_ctrl_z(int s)
+{
+    if (running) {
+        printf("\tCtrl+Z - continue\n\tCtrl+C - terminate\n");
+    } else {
+        printf("\n");
+    }
+
+    running = 1 - running;
+}
+
+int main(int argc, char* argv[])
+{
+    signal(SIGINT, handle_ctrl_c);
+
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(struct sigaction));
+    sa.sa_handler = handle_ctrl_z;
+    sigaction(SIGTSTP, &sa, NULL);
+
+    time_t t;
+    struct tm tm;
+
+    while (1) {
+        if (running) {
+            time(&t);
+            tm = *localtime(&t);
+            printf("%d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900,
+                tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+        }
+
+        usleep(500000);
+    }
+
+    return 0;
+}
